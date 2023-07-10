efdf4f47f4819f464dd0af0bd19eac5a3598d8bc
NineTwoProgramming <marasimowicz@ymail.com>
2023-07-09 20:28:31 -0400

    added more choices to view and update the database

--- server.js
+++ server.js
@@ -1,35 +1,59 @@
 import connection from "./lib/connection.js";
-import inquirer from 'inquirer';
-import utils from "./lib/utils.js";
+import inquirer from "inquirer";
+import utils from "./lib/queries.js";
 
-connection.connect((error) => {
-  if(error) throw error;
-  console.log('Connected to MySQL server.')
-})
+(async () => {
+  try {
+    const data = await inquirer.prompt([
+      {
+        type: "list",
+        name: "choice",
+        message: "What would you like to do?",
+        choices: [
+          "View All Employees",
+          "View All Departments",
+          "View All Roles",
+          "Add Department",
+          "Exit Program",
+        ],
+      },
+    ]);
 
-inquirer
-  .prompt([
-  {
-    type: "list",
-    name: "choice",
-    message: "What would you like to do?",
-    choices: ["View All Employees", "Choice 2", "Choice 3", "quit"],
-  },
-])
-.then((data) => {
-  console.log(data.choice);
-    switch (data.choice){
-        case "View All Employees":
-                utils.viewAllEmp(connection);
-                break;
-        default:
+    switch (data.choice) {
+      case "View All Employees":
+        utils.viewAllEmp();
+        break;
+      case "View All Departments":
+        utils.viewAllDep();
+        break;
+      case "View All Roles":
+        utils.viewAllRoles();
+        break;
+      case "Add Department":
+        inquirer
+          .prompt([
+            {
+              type: "input",
+              name: "name",
+              message: "Which department would you like to add?",
+            },
+          ])
+          .then(async function (answer) {
+            await utils.addDepartment(answer.name);
+            console.log(`${answer.name} has been added to departments.`);
+            await connection.end();
+          });
+        break;
+      case "Exit Program":
+          console.log("Goodbye!");
+          connection.end();
+          break;
+      default:
         console.log("Invalid choice");
+        await connection.end();
     }
-})
-.catch((error) => {
-  if (error.isTtyError) {
-    // Prompt couldn't be rendered in the current environment
-  } else {
+  } catch (error) {
     console.log(error);
+    await connection.end();
   }
-});
+})();
