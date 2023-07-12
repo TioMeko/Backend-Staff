b80c89be96a09db59cde243565e139d47f09a9b0
NineTwoProgramming <marasimowicz@ymail.com>
2023-07-11 21:41:24 -0400

    updated schema

--- db/schema.sql
+++ db/schema.sql
@@ -14,7 +14,7 @@ CREATE TABLE role (
     title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
-    PRIMARY KEY (id)
+    PRIMARY KEY (id),
     FOREIGN KEY (department_id) REFERENCES department(id)
 );
 
@@ -23,7 +23,7 @@ CREATE TABLE employee (
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    manager_id INT,
-    PRIMARY KEY (id)
+    manager_id INT NULL,
+    PRIMARY KEY (id),
     FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
